24632b8927acf22d304c087892646b63f1c3e19f
Islam Ahmed <142545372+SoftEng-Islam@users.noreply.github.com>
2023-12-22 04:21:44 +0200

    update

--- server/index.js
+++ server/index.js
@@ -1,13 +1,52 @@
+// import express from "express";
+// const port = process.env.PORT || 3000;
+// const app = express();
+// app.get("/api/v1/hello", (_req, res) => {
+// 	res.json({ message: "Hello, Islam!" });
+// });
+// app.listen(port, () => {
+// 	console.log("Server listening on port", port);
+// });
+
+
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 import express from "express";
-
-const port = process.env.PORT || 3000;
-
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
-app.get("/api/v1/hello", (_req, res) => {
-	res.json({ message: "Hello, Islam!" });
+
+
+var corsOptions = {
+	origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+const db = require("./app/models");
+db.sequelize.sync();
+
+
+// simple route
+app.get("/", (req, res) => {
+	res.json({ message: "Welcome to my application." });
 });
 
-app.listen(port, () => {
-	console.log("Server listening on port", port);
+
+
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+	console.log(`http://localhost:${PORT}`);
 });
