1d027e49e4649121d226fd47ea9b9788e9214816
Islam Ahmed <142545372+SoftEng-Islam@users.noreply.github.com>
2023-12-24 03:03:03 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,11 @@
-import { rmSync } from "node:fs";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import pkg from "./package.json";
-import { URL } from "node:url";
 import vuePugPlugin from "vue-pug-plugin";
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import { VitePluginNode } from "vite-plugin-node";
 
 
 export default defineConfig(({ command }) => {
-	rmSync("dist", { recursive: true, force: true });
-	const isServe = command === "serve";
-	const isBuild = command === "build";
-	const sourcemap = isServe || !!process.env.VSCODE_DEBUG;
 	return {
 		plugins: [
 			vue({
@@ -31,16 +24,9 @@ export default defineConfig(({ command }) => {
 				swcOptions: {},
 			}),
 		],
-		server:
-		process.env.VSCODE_DEBUG &&
-		(() => {
-			const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL);
-			return {
-				host: url.hostname,
-				port: +url.port,
-			};
-		})(),
-		clearScreen: false,
+		server: {
+			port: 1420,
+			strictPort: true,
+		}
 	}
-
 });
