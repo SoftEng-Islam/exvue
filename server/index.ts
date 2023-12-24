5b342598ddd3ab3c70fe88f80dca3b004aaa95fb
Islam Ahmed <142545372+SoftEng-Islam@users.noreply.github.com>
2023-12-25 00:27:39 +0200

    update

--- server/index.ts
+++ server/index.ts
@@ -1,45 +1,68 @@
-// import { createRequire } from 'module';
-// const require = createRequire(import.meta.url);
+import express, { Application } from "express";
+import cors, { CorsOptions } from "cors";
+import Routes from "./routes";
+import Database from "./db";
 
-import express, { Express, Request, Response } from "express";
+export default class Server {
+	constructor(app: Application) {
+		this.config(app);
+		this.syncDatabase();
+		new Routes(app);
+	}
 
-const app: Express = express();
+	private config(app: Application): void {
+		const corsOptions: CorsOptions = {
+			origin: "http://localhost:8081",
+		};
 
-// // set port, listen for requests
-const PORT: string | 8080 = process.env.PORT || 8080;
+		app.use(cors(corsOptions));
+		app.use(express.json());
+		app.use(express.urlencoded({ extended: true }));
+	}
 
-import bodyParser from "body-parser";
-import cors from "cors";
+	private syncDatabase(): void {
+		const db = new Database();
+		db.sequelize?.sync();
+	}
+}
 
+// // import { createRequire } from 'module';
+// // const require = createRequire(import.meta.url);
 
+// import express, { Express, Request, Response } from "express";
 
-var corsOptions = {
-	origin: "http://localhost:8081"
-};
+// const app: Express = express();
 
-app.use(cors(corsOptions));
+// // // set port, listen for requests
+// const PORT: string | 8080 = process.env.PORT || 8080;
 
-// // parse requests of content-type - application/json
-app.use(bodyParser.json());
+// import bodyParser from "body-parser";
+// import cors from "cors";
 
-// // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+// var corsOptions = {
+// 	origin: "http://localhost:8081"
+// };
 
+// app.use(cors(corsOptions));
 
-// import db from "./models/index.js";
-// db.sequelize.sync({ force: true }).then(() => {
-// 	console.log("Drop and re-sync db.");
+// // // parse requests of content-type - application/json
+// app.use(bodyParser.json());
+
+// // // parse requests of content-type - application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// // import db from "./models/index.js";
+// // db.sequelize.sync({ force: true }).then(() => {
+// // 	console.log("Drop and re-sync db.");
+// // });
+
+// // // simple route
+// app.get("/", (req: Request, res: Response) => {
+// 	res.json({ message: "Welcome to my application." });
 // });
 
+// app.listen(PORT, () => {
+// 	console.log(`http://localhost:${PORT}`);
+// });
 
-// // simple route
-app.get("/", (req: Request, res: Response) => {
-	res.json({ message: "Welcome to my application." });
-});
-
-
-app.listen(PORT, () => {
-	console.log(`http://localhost:${PORT}`);
-});
-
-export const viteNodeApp = app;
+// export const viteNodeApp = app;
